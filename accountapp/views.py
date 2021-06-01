52b4c07ea212ec28ce32905546623404272977b5
inyoung bae <inyoung.bae@play3s.com>
2021-06-01 16:38:51 +0900

    course 25 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView
 
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
@@ -36,3 +37,14 @@ class AccountCreateView(CreateView):
     # reverse / reverse_lazy
     # reverse는 클래스에서 사용할 수 없음
     template_name = 'accountapp/create.html' #회원가입시 보여줄 form 어느 html 파일을 통해 보여줄지
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'

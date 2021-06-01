6c03c591e352f0b9602ffe6eceda0c87241bfd3a
inyoung bae <inyoung.bae@play3s.com>
2021-06-01 10:02:45 +0900

    course 21 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,11 @@
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -20,9 +22,17 @@ def hello_world(request):
 
         hello_world_list = HelloWorld.objects.all()
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
-            # render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
 
     else:
 
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world') #성공 후 어느경로로 재 연결?
+    # reverse / reverse_lazy
+    # reverse는 클래스에서 사용할 수 없음
+    template_name = 'accountapp/create.html' #회원가입시 보여줄 form 어느 html 파일을 통해 보여줄지
